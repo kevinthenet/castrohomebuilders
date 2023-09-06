8a3277902d06c969e106f847f055951091fc9f8b
Kevin Castro <me@kevincastro.dev>
2023-09-06 16:48:18 -0700

    try removing compress integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,9 @@ import { defineConfig } from 'astro/config';
 import robotsTxt from 'astro-robots-txt';
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://castrohomebuilders.com",
-  integrations: [robotsTxt(), sitemap(), tailwind(), compress()]
+  integrations: [robotsTxt(), sitemap(), tailwind()]
 });
