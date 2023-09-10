31bca4b4b37b0b0dfb24c548ed70f5f35ba641b4
Kevin Castro <me@kevincastro.dev>
2023-09-10 12:20:12 -0700

    Revert "Revert "setup submit endpoint & hook up with contact form""
    This reverts commit ea0a6b22c6bd4ad7149233b4cd65ba080fce4ff3.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,14 @@ import robotsTxt from 'astro-robots-txt';
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
+import nodejs from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://castrohomebuilders.com",
-  integrations: [robotsTxt(), sitemap(), tailwind(), compress()]
+  integrations: [robotsTxt(), sitemap(), tailwind(), compress()],
+  output: 'hybrid',
+  adapter: nodejs({
+    mode: 'middleware'
+  }),
 });
