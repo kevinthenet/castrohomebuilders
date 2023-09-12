6c4a95df4219955af4daa48cfbb0f23f7398906f
Kevin Castro <me@kevincastro.dev>
2023-09-12 11:59:07 -0700

    remove cloudflare, add astro-webmanifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@ import { defineConfig } from 'astro/config';
 import robotsTxt from 'astro-robots-txt';
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import webmanifest from "astro-webmanifest";
 import compress from "astro-compress";
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://castrohomebuilders.com",
-  integrations: [robotsTxt(), sitemap(), tailwind(), compress()],
-  output: 'hybrid',
-  adapter: cloudflare({ mode: 'directory' }),
+  integrations: [robotsTxt(), sitemap(), tailwind(), webmanifest({
+    name: 'Castro Home Builders',
+    icon: 'public/chb-logo.png',
+    description: 'Castro Home Builders is a contracting company operating in the Bay Area with over 20 years of experience',
+    start_url: '/',
+  }), compress(),],
 });
