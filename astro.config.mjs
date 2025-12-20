527964459240343a95c681d14bd6bb9333523c02
Kevin Castro <me@kevincastro.dev>
2025-12-19 17:00:10 -0800

    fix up alpinejs usage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,26 @@
 import { defineConfig } from 'astro/config';
 import robotsTxt from 'astro-robots-txt';
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import webmanifest from "astro-webmanifest";
-import compress from "astro-compress";
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import webmanifest from 'astro-webmanifest';
+import compress from 'astro-compress';
+import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://castrohomebuilders.com",
-  integrations: [robotsTxt(), sitemap(), tailwind(), webmanifest({
-    name: 'Castro Home Builders',
-    icon: 'public/chb-logo.png',
-    description: 'Castro Home Builders is a contracting company operating in the Bay Area with over 20 years of experience',
-    start_url: '/',
-  }), compress(),],
+  site: 'https://castrohomebuilders.com',
+  integrations: [
+    robotsTxt(),
+    sitemap(),
+    tailwind(),
+    webmanifest({
+      name: 'Castro Home Builders',
+      icon: 'public/chb-favicon-light.png',
+      description:
+        'Castro Home Builders is a general contracting company operating in the Bay Area with over 20 years of experience',
+      start_url: '/',
+    }),
+    compress(),
+    alpinejs(),
+  ],
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,6 @@
 /// <reference types="astro/client" />
 declare module 'canvas-confetti';
+
+interface Window {
+  Alpine: import('alpinejs').Alpine;
+}
